3faf6b2c146ab67f4a767edd4dd187ceadd48139
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2020-10-18 02:04:24 -0700

    udpate

--- basic/and-gate/and-gate.v
+++ basic/and-gate/and-gate.v
@@ -1,15 +1,32 @@
-// AND gate
+// AND gate designed a few different ways
 module and_gate(
-    x,      // in1
-    y,      // in2
-    xy      // out
+    x,        // in x
+    y,        // in y
+    xy_1      // out 1 - ???
+    xy_2      // out 2 - ???
+    xy_3      // out 3 - ???
+    xy_4      // out 4 - ???
 );
 
 // PORT DECLARATION
 input x, y;
-output xy;
+output xy_0, xy_1, xy_3, xy_4;
+
+// DATA TYPES (NOT NEEDED IN THIS EXAMPLE)   
+wire x, y;
+wire xy_0, xy_1, xy_3, xy_4;
 
 // CODE STARTS HERE
-and(xy, x, y);
 
+// METHOD 1 - BITWISE AND OPERATOR
+
+// METHOD 2 - ???
+
+// METHOD 3 - ???
+
+// METHOD 4 - ???
+
+
+and(xy, x, y);
+xy <= x + y
 endmodule
