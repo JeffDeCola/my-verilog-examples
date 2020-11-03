75214f57aa42f8254d0a02bfb2b20e00fe1e04fb
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2020-11-03 13:55:54 -0800

    update

--- fpga-development-boards/buttons/buttons/buttons.v
+++ fpga-development-boards/buttons/buttons/buttons.v
@@ -1,20 +1,20 @@
 // a few buttons
 
 // PULSE HIGH FOR 1 CLOCK CYCLE WHEN BUTTON PRESSED
-module button_clock_pulse(
+module button_clock_pulse (
     input           clk,            // CLOCK
     input           pressed,        // BUTTON IN (YOU CAN HOLD AND RELEASE WHENEVER)
     output reg      out             // PULSE FOR 1 CLOCK CYCLE
 );
 
-reg button_pressed = 0;
+reg lock = 0;
 
 always @ (posedge clk) begin
-    if (pressed & ~button_pressed) begin
-        button_pressed <= 1'b1;
-        out <= 1'b1;
-    end else if (button_pressed & ~pressed) begin
-        button_pressed <= 1'b0;
+    if (pressed & ~lock) begin              // BUTTON PRESSED
+        lock <= 1'b1;                       // - Lock
+        out <= 1'b1;                        // - Pulse
+    end else if (~pressed & lock) begin     // BUTTON RELEASED
+        lock <= 1'b0;                       // - Release Lock
     end else begin
         out <= 1'b0;
     end
@@ -23,23 +23,23 @@ end
 endmodule
 
 // TWO PRESSES - FIRST PRESS HIGH, SECOND PRESS LOW
-module button_two_presses(
+module button_two_presses (
     input           clk,            // CLOCK
     input           pressed,        // BUTTON IN (YOU CAN HOLD AND RELEASE WHENEVER)
-    output          out             // 
+    output          out             // TWO PRESSES
 );
 
-reg button_pressed;
-reg out = 0;
+reg lock = 0;
+reg toggle = 0;
 
-always @ (posedge clk or posedge pressed) begin
-    if (pressed & ~button_pressed) begin
-        button_pressed <= 1'b1;
-        out <= 1'b1;
-    end else if (button_pressed) begin
-        button_pressed <= 1'b0;
-    end else begin
-        out <= 1'b1;
+assign out = toggle;
+
+always @ (posedge clk) begin
+    if (pressed & ~lock) begin              // BUTTON PRESSED
+        lock <= 1'b1;                       // - Lock
+        toggle <= ~toggle;                   // - toggle
+    end else if (~pressed & lock) begin     // BUTTON RELEASED
+        lock <= 1'b0;                       // - Release Lock
     end
 end
 
