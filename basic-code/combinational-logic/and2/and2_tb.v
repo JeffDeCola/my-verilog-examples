804ee4ba8d4c4b213e4d30f86d821e1c1bf6ffa9
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-04-06 20:19:19 -0400

    changing filenames to reflect module names

---
+++ basic-code/combinational-logic/and2/and2_tb.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ns
+
+// include files are in and2.vh
+
+module and2_tb;
+
+    // DATA TYPES - DECLAIR REGISTERS AND WIRES (PROBES)
+    reg        A, B;
+    wire       Y;
+
+    // UNIT UNDER TEST
+    and2 uut(
+        .a(A), .b(B),
+        .y(Y)
+    );
+
+    // SAVE EVERYTHING FROM TOP MODULE IN A DUMP FILE
+    initial begin
+        $dumpfile("and2_tb.vcd");
+        $dumpvars(0, and2_tb);
+    end
+
+    // TESTCASE - CHANGE REG VALUES
+    initial begin
+        $display("test start");
+        B = 0; A = 0;
+
+        #15; B = 0; A = 0;
+        #20; B = 0; A = 1;
+        #20; B = 1; A = 0;
+        #20; B = 1; A = 1;
+
+        #20; B = 0; A = 0;
+
+        // DONE
+        #20
+
+        $display("test complete");
+        $finish;
+    end
+
+endmodule
